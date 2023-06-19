8c605368cb22f3b1393b4f97f593917d65f79c73
emmiefroshe <42306531+emmiefroshe@users.noreply.github.com>
2023-06-19 23:49:15 +0100

    Add files via upload

--- JSDom.js
+++ JSDom.js
@@ -2,3 +2,10 @@ function myFunction(){
     var element = document.body;
     element.classList.toggle("dark-mode");
 }
+
+const bookList = document.querySelector("#book-list");
+console.log(bookList);
+
+
+const banner = document.querySelector("#page-banner");
+console.log('The banner type is:', banner.nodeType);
