c81e805ff3d2fac14f34e2a032e03122326a21d7
emmiefroshe <42306531+emmiefroshe@users.noreply.github.com>
2023-06-20 01:02:09 +0100

    Add files via upload
    added a sample class

--- JSDom.js
+++ JSDom.js
@@ -3,9 +3,34 @@ function myFunction(){
     element.classList.toggle("dark-mode");
 }
 
-const bookList = document.querySelector("#book-list");
-console.log(bookList);
+// const bookList = document.querySelector("#book-list");
+// console.log(bookList);
 
 
-const banner = document.querySelector("#page-banner");
-console.log('The banner type is:', banner.nodeType);
+// const banner = document.querySelector("#page-banner");
+// console.log('The banner type is:', banner.nodeType);
+
+
+// I'm attempting to create a class named "Book" 
+class Book {
+    constructor(name) {
+      this.name = name;
+    }
+  
+    delete() {
+      console.log(`Deleting book: {this.name}`);
+    }
+  }
+  
+  // UI class
+  class UI {
+    static addBook(bookName) {
+      const book = new Book(bookName);
+      console.log(`Adding book: {book.name}`);
+    }
+  }
+  
+  // METHODS
+  UI.addBook('Name of the Wind');
+  UI.addBook('The Wise Man\'s Fear');
+  
